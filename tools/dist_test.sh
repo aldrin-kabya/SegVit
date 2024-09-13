64330a74ba92981e59171c1998f6594e8c4b5a67
aldrin-kabya <aldrin.kabya04@gmail.com>
2024-09-14 05:53:36 +0600

    single gpu testing

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -5,4 +5,4 @@ CHECKPOINT=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python $(dirname "$0")/test.py $CONFIG $CHECKPOINT --eval mIoU  --launcher pytorch ${@:3}
+python $(dirname "$0")/test.py $CONFIG $CHECKPOINT --eval mIoU  --launcher none
