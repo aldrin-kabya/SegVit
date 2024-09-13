f3f2c8559ab83d396ccc4c7cc690c6f15f66d8de
aldrin-kabya <aldrin.kabya04@gmail.com>
2024-09-14 05:48:21 +0600

    single gpu testing

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -5,5 +5,4 @@ CHECKPOINT=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=$((RANDOM + 10000)) \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --eval mIoU  --launcher pytorch ${@:3}
+python $(dirname "$0")/test.py $CONFIG $CHECKPOINT --eval mIoU  --launcher pytorch ${@:3}
