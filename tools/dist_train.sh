41ddac9024722c705e8c721dc632415afc2b4a3c
aldrin-kabya <aldrin.kabya04@gmail.com>
2024-09-14 01:19:56 +0600

    single gpu modification

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -5,5 +5,4 @@ WORK_DIR=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=$((RANDOM + 10000)) \
-    $(dirname "$0")/train.py $CONFIG --work-dir $WORK_DIR --launcher pytorch ${@:3}
+python $(dirname "$0")/train.py $CONFIG --work-dir $WORK_DIR ${@:3}
