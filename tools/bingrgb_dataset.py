5cdfc35582484770bf46b523790d6d7f83e4a44f
aldrin-kabya <aldrin.kabya04@gmail.com>
2024-09-21 03:42:40 +0600

    modified for bingrgb dataset

--- tools/bingrgb_dataset.py
+++ tools/bingrgb_dataset.py
@@ -17,35 +17,35 @@ class BingRGBDataset(CustomDataset):
     #     [255, 251, 240], [128, 0, 128]
     # ]
 
-    # 1st selected classes and pallete
-    CLASSES = (
-        'background', 'farmland', 'water', 'forest', 
-        'urban_structure', 'meadow'
-    )
-    
-    PALETTE = [
-        [0, 0, 0],    # background
-        [0, 255, 0],  # farmland
-        [0, 0, 255],  # water
-        [0, 255, 255],# forest
-        [128, 0, 0],  # urban_structure (merged with rural_built_up, urban_built_up, road, and brick_factory)
-        [255, 255, 0] # meadow (merged with marshland)
-    ]
-
-    # # BDSAT Paper classes and pallete
+    # # 1st selected classes and pallete
     # CLASSES = (
-    #     'background', 'forest', 'urban_structure', 'water', 
-    #     'farmland', 'meadow'
+    #     'background', 'farmland', 'water', 'forest', 
+    #     'urban_structure', 'meadow'
     # )
     
     # PALETTE = [
     #     [0, 0, 0],    # background
+    #     [0, 255, 0],  # farmland
+    #     [0, 0, 255],  # water
     #     [0, 255, 255],# forest
     #     [128, 0, 0],  # urban_structure (merged with rural_built_up, urban_built_up, road, and brick_factory)
-    #     [0, 0, 255],  # water
-    #     [0, 255, 0],  # farmland
     #     [255, 255, 0] # meadow (merged with marshland)
     # ]
+
+    # BDSAT Paper classes and pallete
+    CLASSES = (
+        'background', 'forest', 'urban_structure', 'water', 
+        'farmland', 'meadow'
+    )
+    
+    PALETTE = [
+        [0, 0, 0],    # background
+        [0, 255, 255],# forest
+        [128, 0, 0],  # urban_structure (merged with rural_built_up, urban_built_up, road, and brick_factory)
+        [0, 0, 255],  # water
+        [0, 255, 0],  # farmland
+        [255, 255, 0] # meadow (merged with marshland)
+    ]
     
     def __init__(self, **kwargs):
         # Remove `reduce_zero_label` here since it will be provided by the configuration
